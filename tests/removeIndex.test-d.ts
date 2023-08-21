c3c046fef82d9aad3e2a42412c05d1a78de04d5f
PCOffline <bekerman122@gmail.com>
2023-08-21 23:52:13 +0300

    test: add tests for removeIndex

---
+++ tests/removeIndex.test-d.ts
@@ -0,0 +1,20 @@
+import { expectTypeOf } from 'expect-type';
+import { RemoveIndex } from '../src/util.js';
+
+class Input {
+  [x: string]: any
+  [x: number]: any
+  [x: symbol]: any
+  [x: `head-${string}`]: string
+  [x: `${string}-tail`]: string
+  [x: `head-${string}-tail`]: string
+  [x: `${bigint}`]: string
+  [x: `embedded-${number}`]: string
+
+  normal = 123;
+  optional?: string;
+}
+
+type Result = RemoveIndex<Input>;
+
+expectTypeOf<Result>().toEqualTypeOf<{ normal: number; optional?: string; }>();
