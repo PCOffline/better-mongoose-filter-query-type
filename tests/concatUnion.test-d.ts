c60439dbccb15a8dcc6deb5a826afd01bbc02b45
PCOffline <bekerman122@gmail.com>
2023-08-21 23:55:39 +0300

    test: add tests for concat union

---
+++ tests/concatUnion.test-d.ts
@@ -0,0 +1,9 @@
+import { ConcatUnion } from '../src/util.js';
+import { expectTypeOf } from 'expect-type';
+
+expectTypeOf<ConcatUnion<'a', 'b'>>().toEqualTypeOf<'a' | 'ab'>();
+expectTypeOf<ConcatUnion<'a', 1>>().toEqualTypeOf<'a' | 'a1'>();
+expectTypeOf<ConcatUnion<'a', true>>().toEqualTypeOf<'a' | 'atrue'>();
+expectTypeOf<ConcatUnion<1, 'b'>>().toEqualTypeOf<1 | '1b'>();
+expectTypeOf<ConcatUnion<true, 'b'>>().toEqualTypeOf<true | 'trueb'>();
+expectTypeOf<ConcatUnion<`${number}-a`, 'b'>>().toEqualTypeOf<`${number}-a` | `${number}-ab`>();
