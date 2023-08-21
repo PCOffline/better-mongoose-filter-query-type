90c2ca3947a7bd555972c660497e7977dba6a3c7
PCOffline <bekerman122@gmail.com>
2023-08-22 02:09:39 +0300

    test(getFieldInArrayByPath): add tests for length and invalid keys

--- tests/utility-tests/getFieldInArrayByPath.test-d.ts
+++ tests/utility-tests/getFieldInArrayByPath.test-d.ts
@@ -5,40 +5,132 @@ import { expectTypeOf } from 'expect-type';
 //                              Indexes
 // ----------------------------------------------------------------
 
+// Empty Array
+expectTypeOf<GetFieldInArrayByPath<[], '0'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<[], '1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<[], '999'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<[], 'length'>>().toEqualTypeOf<0>();
+
 // Primitives
 expectTypeOf<GetFieldInArrayByPath<string[], '0'>>().toBeString();
 expectTypeOf<GetFieldInArrayByPath<string[], '1'>>().toBeString();
 expectTypeOf<GetFieldInArrayByPath<string[], '999'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<string[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<number[], '0'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<number[], '1'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<number[], '999'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<number[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<number[], '-1'>>().toBeNever();
 expectTypeOf<GetFieldInArrayByPath<boolean[], '0'>>().toBeBoolean();
 expectTypeOf<GetFieldInArrayByPath<boolean[], '1'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<boolean[], '-1'>>().toBeNever();
 expectTypeOf<GetFieldInArrayByPath<boolean[], '999'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<boolean[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<null[], '0'>>().toBeNull();
 expectTypeOf<GetFieldInArrayByPath<null[], '1'>>().toBeNull();
 expectTypeOf<GetFieldInArrayByPath<null[], '999'>>().toBeNull();
+expectTypeOf<GetFieldInArrayByPath<null[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<null[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<undefined[], '0'>>().toBeUndefined();
 expectTypeOf<GetFieldInArrayByPath<undefined[], '1'>>().toBeUndefined();
 expectTypeOf<GetFieldInArrayByPath<undefined[], '999'>>().toBeUndefined();
+expectTypeOf<GetFieldInArrayByPath<undefined[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<undefined[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<symbol[], '0'>>().toBeSymbol();
 expectTypeOf<GetFieldInArrayByPath<symbol[], '1'>>().toBeSymbol();
 expectTypeOf<GetFieldInArrayByPath<symbol[], '999'>>().toBeSymbol();
+expectTypeOf<GetFieldInArrayByPath<symbol[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<symbol[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<never[], '0'>>().toBeNever();
 expectTypeOf<GetFieldInArrayByPath<never[], '1'>>().toBeNever();
 expectTypeOf<GetFieldInArrayByPath<never[], '999'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<never[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<never[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<unknown[], '0'>>().toBeUnknown();
 expectTypeOf<GetFieldInArrayByPath<unknown[], '1'>>().toBeUnknown();
 expectTypeOf<GetFieldInArrayByPath<unknown[], '999'>>().toBeUnknown();
+expectTypeOf<GetFieldInArrayByPath<unknown[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<unknown[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<void[], '0'>>().toBeVoid();
 expectTypeOf<GetFieldInArrayByPath<void[], '1'>>().toBeVoid();
 expectTypeOf<GetFieldInArrayByPath<void[], '999'>>().toBeVoid();
+expectTypeOf<GetFieldInArrayByPath<void[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<void[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<object[], '0'>>().toBeObject();
 expectTypeOf<GetFieldInArrayByPath<object[], '1'>>().toBeObject();
 expectTypeOf<GetFieldInArrayByPath<object[], '999'>>().toBeObject();
+expectTypeOf<GetFieldInArrayByPath<object[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<object[], 'length'>>().toBeNumber();
 expectTypeOf<GetFieldInArrayByPath<any[], '0'>>().toBeAny();
 expectTypeOf<GetFieldInArrayByPath<any[], '1'>>().toBeAny();
 expectTypeOf<GetFieldInArrayByPath<any[], '999'>>().toBeAny();
+expectTypeOf<GetFieldInArrayByPath<any[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<any[], 'length'>>().toBeNumber();
+
+// Readonly Arrays
+expectTypeOf<GetFieldInArrayByPath<readonly string[], '0'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<readonly string[], '1'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<readonly string[], '999'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<readonly string[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly string[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly number[], '0'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly number[], '1'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly number[], '999'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly number[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly number[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly boolean[], '0'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<readonly boolean[], '1'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<readonly boolean[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly boolean[], '999'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<readonly boolean[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly null[], '0'>>().toBeNull();
+expectTypeOf<GetFieldInArrayByPath<readonly null[], '1'>>().toBeNull();
+expectTypeOf<GetFieldInArrayByPath<readonly null[], '999'>>().toBeNull();
+expectTypeOf<GetFieldInArrayByPath<readonly null[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly null[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly undefined[], '0'>>().toBeUndefined();
+expectTypeOf<GetFieldInArrayByPath<readonly undefined[], '1'>>().toBeUndefined();
+expectTypeOf<GetFieldInArrayByPath<readonly undefined[], '999'>>().toBeUndefined();
+expectTypeOf<GetFieldInArrayByPath<readonly undefined[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly undefined[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly symbol[], '0'>>().toBeSymbol();
+expectTypeOf<GetFieldInArrayByPath<readonly symbol[], '1'>>().toBeSymbol();
+expectTypeOf<GetFieldInArrayByPath<readonly symbol[], '999'>>().toBeSymbol();
+expectTypeOf<GetFieldInArrayByPath<readonly symbol[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly symbol[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly never[], '0'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly never[], '1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly never[], '999'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly never[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly never[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly unknown[], '0'>>().toBeUnknown();
+expectTypeOf<GetFieldInArrayByPath<readonly unknown[], '1'>>().toBeUnknown();
+expectTypeOf<GetFieldInArrayByPath<readonly unknown[], '999'>>().toBeUnknown();
+expectTypeOf<GetFieldInArrayByPath<readonly unknown[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly unknown[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly void[], '0'>>().toBeVoid();
+expectTypeOf<GetFieldInArrayByPath<readonly void[], '1'>>().toBeVoid();
+expectTypeOf<GetFieldInArrayByPath<readonly void[], '999'>>().toBeVoid();
+expectTypeOf<GetFieldInArrayByPath<readonly void[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly void[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly object[], '0'>>().toBeObject();
+expectTypeOf<GetFieldInArrayByPath<readonly object[], '1'>>().toBeObject();
+expectTypeOf<GetFieldInArrayByPath<readonly object[], '999'>>().toBeObject();
+expectTypeOf<GetFieldInArrayByPath<readonly object[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly object[], 'length'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<readonly any[], '0'>>().toBeAny();
+expectTypeOf<GetFieldInArrayByPath<readonly any[], '1'>>().toBeAny();
+expectTypeOf<GetFieldInArrayByPath<readonly any[], '999'>>().toBeAny();
+expectTypeOf<GetFieldInArrayByPath<readonly any[], '-1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<readonly any[], 'length'>>().toBeNumber();
+
+// Readonly Arrays With Specific Indexes
+expectTypeOf<GetFieldInArrayByPath<[string, number]>, '0'>().toEqualTypeOf<string>();
+expectTypeOf<GetFieldInArrayByPath<[string, number]>, '1'>().toEqualTypeOf<number>();
+expectTypeOf<GetFieldInArrayByPath<[string, number]>, '2'>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<[string, number]>, 'length'>().toEqualTypeOf<2>();
 
 // Unions
 expectTypeOf<GetFieldInArrayByPath<Array<string | number>, '0'>>().toEqualTypeOf<string | number>();
@@ -69,18 +161,18 @@ expectTypeOf<GetFieldInArrayByPath<Array<{ x: number[] }>, '999.x.1'>>().toBeNum
 expectTypeOf<GetFieldInArrayByPath<Array<{ x: number[] }>, '999.x.999'>>().toBeNumber();
 
 // Two-Dimensional Arrays
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '0'>>().toEqualTypeOf<Array<string>>();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '1'>>().toEqualTypeOf<Array<string>>();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '999'>>().toEqualTypeOf<Array<string>>();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '0.0'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '0.1'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '0.999'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '1.0'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '1.1'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '1.999'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '999.0'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '999.1'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Array<string>>, '999.999'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '0'>>().toEqualTypeOf<string[]>();
+expectTypeOf<GetFieldInArrayByPath<string[][], '1'>>().toEqualTypeOf<string[]>();
+expectTypeOf<GetFieldInArrayByPath<string[][], '999'>>().toEqualTypeOf<string[]>();
+expectTypeOf<GetFieldInArrayByPath<string[][], '0.0'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '0.1'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '0.999'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '1.0'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '1.1'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '1.999'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '999.0'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '999.1'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<string[][], '999.999'>>().toBeString();
 
 // ----------------------------------------------------------------
 //                    Accessing Fields Directly
@@ -118,28 +210,28 @@ interface Input2 {
   z: boolean;
 }
 
-expectTypeOf<GetFieldInArrayByPath<Array<Input1>, 'x'>>().toEqualTypeOf<{ y: string; }>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input1>, 'x.y'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Input1>, 'z'>>().toBeBoolean();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x'>>().toEqualTypeOf<Input2['x']>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.y'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested'>>().toEqualTypeOf<Input2['x']['nested']>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.a'>>().toBeNumber();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.b'>>().toBeString();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.c'>>().toEqualTypeOf<boolean[]>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.c.0'>>().toBeBoolean();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.c.1'>>().toBeBoolean();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.c.999'>>().toBeBoolean();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d'>>().toEqualTypeOf<Input2['x']['nested']['d']>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d._'>>().toBeBoolean();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$'>>().toEqualTypeOf<Input2['x']['nested']['d']['$']>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.0'>>().toEqualTypeOf<Input2['x']['nested']['d']['$'][number]>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.1'>>().toEqualTypeOf<Input2['x']['nested']['d']['$'][number]>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.999'>>().toEqualTypeOf<Input2['x']['nested']['d']['$'][number]>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.field1'>>().toBeNever();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.field2'>>().toBeVoid();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.field3'>>().toEqualTypeOf<unknown[]>();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.field3.0'>>().toBeUnknown();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.field3.1'>>().toBeUnknown();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'x.nested.d.$.field3.999'>>().toBeUnknown();
-expectTypeOf<GetFieldInArrayByPath<Array<Input2>, 'z'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<Input1[], 'x'>>().toEqualTypeOf<{ y: string; }>();
+expectTypeOf<GetFieldInArrayByPath<Input1[], 'x.y'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<Input1[], 'z'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x'>>().toEqualTypeOf<Input2['x']>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.y'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested'>>().toEqualTypeOf<Input2['x']['nested']>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.a'>>().toBeNumber();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.b'>>().toBeString();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.c'>>().toEqualTypeOf<boolean[]>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.c.0'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.c.1'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.c.999'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d'>>().toEqualTypeOf<Input2['x']['nested']['d']>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d._'>>().toBeBoolean();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$'>>().toEqualTypeOf<Input2['x']['nested']['d']['$']>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.0'>>().toEqualTypeOf<Input2['x']['nested']['d']['$'][number]>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.1'>>().toEqualTypeOf<Input2['x']['nested']['d']['$'][number]>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.999'>>().toEqualTypeOf<Input2['x']['nested']['d']['$'][number]>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.field1'>>().toBeNever();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.field2'>>().toBeVoid();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.field3'>>().toEqualTypeOf<unknown[]>();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.field3.0'>>().toBeUnknown();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.field3.1'>>().toBeUnknown();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'x.nested.d.$.field3.999'>>().toBeUnknown();
+expectTypeOf<GetFieldInArrayByPath<Input2[], 'z'>>().toBeBoolean();
